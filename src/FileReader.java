dd7d66d59fb268009d1a01b7942fc0247b7733a1
Winarto <yowinarto@gmail.com>
2017-03-31 13:43:28 +0700

    First commit

---
+++ src/FileReader.java
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 03/31/2017.
+ */
+public class FileReader {
+  private String fileName;
+
+  FileReader(String inFileName) {
+    fileName = inFileName;
+  }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 03/31/2017.
+ */
+public class Main {
+  FileReader file = new FileReader("matriks.txt");
+}
